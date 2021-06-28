b65751ddfa1dae4655664511716e67b7f7570730
Oliver Benning <me@oliverbenning.com>
2021-06-28 19:25:48 +0000

    Initial content round

--- tree.js
+++ tree.js
@@ -4,6 +4,8 @@ var chart_config = {
         rootOrientation: "WEST",
 
         animateOnInit: false,
+        hideRootNode: false,
+        connectors: {type: "curve"},
         
         node: {
             collapsable: true
@@ -23,30 +25,192 @@ var chart_config = {
         },
         children: [
             {
-                text: {name: "Developer desktop"},
+                collapsed: true,
+                text: {
+                    title: "Developer desktop",
+                    responsible: "Responsible: Developers",
+                    accountable: "Accountable: Developers",
+                },
                 children: [
                     {
                         text: {
-                            category: "Category",
-                            name: "Child",
+                            name: "Pick secure open source packages so I don’t have to redo a bunch of work",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Reduce pressure/schooling received from security or legal",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Catch issues early and fix them before they become a part of the codebase",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Ensure that code commit will pass through the security gate the first time",
                         }
                     },
                 ]
             },
             {
-                text: {name: "Git integrations"}
+                text: {
+                    title: "Git integrations",
+                    responsible: "Responsible: AppSec",
+                    accountable: "Accountable: Developers",
+                },
+                collapsed: true,
+                children: [
+                    {
+                        text: {
+                            name: "Easy & quick on-boarding to understand overall risk posture (e.g. visibility & reporting",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Monitoring vulnerabilities over time, including 0-days"
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Easy centralized way to action vulnerabilities (automated or manual fixes)",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Early detection of issues pre-merge (in PR tests)",
+                        }
+                    },
+
+                ]
             },
             {
-                text: {name: "CI/CD integration"}
+                text: {
+                    title: "CI/CD integration",
+                    responsible: "Responsible: DevOps",
+                    accountable: "Accountable: AppSec/DevSecOps, Developers",
+                },
+                collapsed: true,
+                children: [
+                    {
+                        text: {
+                            name: "Check for issues and optionally block deployments",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Capture & track dependency details over time (e.g. monitor)",
+                        }
+                    },
+                    {
+                        text: {
+                            name: 'Generate “paper trail”',
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Triage/remediate discovered issues",
+                        }
+                    },
+
+                ]
             },
             {
-                text: {name: "SecOps/SRE (production code)"}
+                text: {
+                    title: "SecOps/SRE (production code)",
+                    responsible: "Responsible: SRE, Incident Response, SOC managers",
+                    accountable: "Accountable: Security Analysts, Developers, Management",
+                },
+                collapsed: true,
+                children: [
+                    {
+                        text: {
+                            name: "Monitor infrastructure & maintain reliability of the production environment (incl 0-days)",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Understand & respond to vulnerabilities based on risk (not just severity) - e.g. prioritize and submit to engineering to fix",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Prevent exploits and attacks on running systems",
+                        }
+                    },
+                ]
             },
             {
-                text: {name: "Issue Management"}
+                text: {
+                    title: "Issue Management",
+                    responsible: "Responsible: SRE, Incident Response, SOC managers",
+                    accountable: "Accountable: Security Analysts, Developers, Management",
+                },
+                collapsed: true,
+                children: [
+                    {
+                        text: {
+                            name: "Prioritize issues that need to be fixed and their respective SLAs based on a policy",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Sync with an external system to trigger and track fixes (JIRA, fix PRs)",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Validate that the fix worked (within SLA)",
+                        }
+                    },
+                    {
+                        text: {
+                            name: 'Approve acceptance of certain risks (“ignore workflow”)',
+                        }
+                    },
+                    {
+                        text: {
+                            name: 'Audit accepted risks and be able to expire “acceptance” (don’t let accepted risk accumulate)',
+                        }
+                    },
+                ]
             },
             {
-                text: {name: "Legal, Governance and Compliance"}
+                text: {
+                    title: "Legal, Governance and Compliance",
+                    responsible: "Responsible: SRE, Incident Response, SOC managers",
+                    accountable: "Accountable: Security Analysts, Developers, Management",
+                },
+                collapsed: true,
+                children: [
+                    {
+                        text: {
+                            name: "Ensure no dependencies with non-compliant licenses reach the production environment, which may put the organization at legal risk",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Track & manage inventory, licenses, issue details and approval trail for each release",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Understand impact of Snyk on the devOps process (ROI)",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Include in broader reporting (feed Snyk data to GRC/SIEM - RSA Archer, Splunk, etc)",
+                        }
+                    },
+                    {
+                        text: {
+                            name: "Help satisfy requirements of a particular compliance standard (e.g. PCI)",
+                        }
+                    },
+
+                ]
             },
         ]
     }
